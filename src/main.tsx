85d5f860844cf5cfd17eaf2d5098edb3957922ea
Tfronn <keeelvincamilo@hotmail.com>
2022-05-09 20:40:45 -0300

    alterações deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,50 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import { createServer, Model } from 'miragejs'
 import '../src/index.css'
 
-createServer({
-  models: {
-    transaction: Model
-  },
 
-  seeds(server) {
-    server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: 'Freelance de website',
-          type: 'deposit',
-          category: 'Dev',
-          amount: 1000,
-          createdAt: new Date('2021-02-12 09:00:00')
-        },
-        {
-          id: 2,
-          title: 'Aluguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 700,
-          createdAt: new Date('2021-02-14 11:00:00')
-        }
-      ]
-    })
-  },
-
-  routes() {
-    this.namespace = 'api'
-    this.get('/transactions', () => {
-      return this.schema.all('transaction')
-    })
-
-    this.post('/transactions', (schema, request) => {
-      const data = JSON.parse(request.requestBody)
-
-      return schema.create('transaction', data)
-    })
-  }
-})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
