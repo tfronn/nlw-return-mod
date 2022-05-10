a2df8399a674d1b579c025b2aa3e5ee64b07ad5a
Tfronn <keeelvincamilo@hotmail.com>
2022-05-10 20:40:02 -0300

    alterações css responsividade 4

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -26,8 +26,8 @@ export function Summary() {
   )
 
   return (
-    <div className="grid grid-cols-3 md:gap-8 -mt-40 sm:gap-2">
-      <div className="bg-white py-6 px-8 rounded-md text-title-500 dark:bg-zinc-800 dark:text-zinc-200 drop-shadow-md">
+    <div className="grid grid-cols-3 md:gap-8 -mt-20 sm:-mt-24 md:-mt-40 sm:gap-2 gap-2 lg:gap-8">
+      <div className="bg-white md:py-4 md:px-6 px-6 py-6 lg:py-6 sm:px-4 lg:px-8 rounded-md text-title-500 dark:bg-zinc-800 dark:text-zinc-200 drop-shadow-md mx-auto w-full">
         <header className="flex align-center justify-between">
           <p className="font-semibold md:text-3xl sm:text-sm">Entradas</p>
           <img src={incomeImg} alt="Entradas" className="sm:h-6 sm:w-6 md:h-8 md:w-8" />
@@ -40,7 +40,7 @@ export function Summary() {
           }).format(summary.deposits)}
         </strong>
       </div>
-      <div className="bg-white py-6 px-8 rounded-md text-title-500 dark:bg-zinc-800 dark:text-zinc-200 drop-shadow-md">
+      <div className="bg-white md:py-4 md:px-6 px-6 py-6 sm:px-4 lg:py-6 lg:px-8 rounded-md text-title-500 dark:bg-zinc-800 dark:text-zinc-200 drop-shadow-md mx-auto w-full">
         <header className="flex align-center justify-between">
           <p className="font-semibold md:text-3xl sm:text-sm">Saídas</p>
           <img src={outcomeImg} alt="Saídas" className="sm:h-6 sm:w-6 md:h-8 md:w-8" />
@@ -53,7 +53,7 @@ export function Summary() {
           }).format(summary.withdraws)}
         </strong>
       </div>
-      <div className="bg-brand-300 py-6 px-8 rounded-md text-white drop-shadow-md">
+      <div className="bg-brand-300 md:py-4 md:px-6 px-6 py-6 sm:px-4 lg:py-6 lg:px-8 rounded-md text-zinc-100 drop-shadow-md mx-auto w-full">
         <header className="flex align-center justify-between">
           <p className="font-semibold md:text-3xl sm:text-sm">Total</p>
           <img src={totalImg} alt="Total" className="sm:h-6 sm:w-6 md:h-8 md:w-8" />
